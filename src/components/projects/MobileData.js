44830555455e1e4c0ad5f2752601835a552c95d7
root <rafaelmerlotto@gmail.com>
2025-02-18 21:00:47 +0100

    Fix mobile view

--- src/components/projects/MobileData.js
+++ src/components/projects/MobileData.js
@@ -7,14 +7,14 @@ export const mobileData = [
         description: " This application was developed in React Native using Expo. With this pizza dough calculator app, you can create your own dough using a simple form. Fill in the\n\
          fields with your desired values, and get the result to start kneading. For saving the dough, and integrating Firebase for data storage and saving user-created dough recipes.",
         img: pizzaCalculator,
-        imgLink: "https://i.imgur.com/WAfkVis.png",
+        imgLink: "https://github.com/rafaelmerlotto/pizza_dough_calculator",
         link: 'https://github.com/rafaelmerlotto/pizza_dough_calculator',
         techUsed: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
         techUsed2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
         techUsed3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-original.svg",
         techUsed4: "https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original.svg",
     },
-    {
+   /*  {
         title: 'Tasks app',
         description: "This app offers a straightforward solution for managing personal tasks, built with React Native using Expo. It includes a feature allowing users to mark tasks\n\
          as important or non-essential by simply tapping a bell icon. Additionally, there are dedicated displays for viewing all tasks and those that have been completed.",
@@ -25,5 +25,5 @@ export const mobileData = [
         techUsed2: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
         techUsed3: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-original.svg",
         techUsed4: "https://raw.githubusercontent.com/devicons/devicon/master/icons/android/android-original.svg",
-    }
+    } */
 ]
