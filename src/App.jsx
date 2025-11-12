d1b09e96b44079e17233b51eb511c7766ed2a239
Rafael <rafaelmerlotto@gmail.com>
2025-11-12 21:11:21 +0100

    Fix App.jsx in the browser routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom/dist";
 import Home from "./pages/Home"
 import "../src/assets/css/style.css"
-import Project from "./pages/Project";
 
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/project" element={<Project />} />
       </Routes>
     </BrowserRouter>
   );
